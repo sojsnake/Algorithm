fff227eb4587211025afcf58e784e79b85c22de1
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-06 22:23:58 +0900

    [Silver III] Title: N번째 큰 수, Time: 2688 ms, Memory: 311216 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = sc.nextInt();
+                pq.add(num); 
+
+                if (pq.size() > n) {
+                    pq.poll(); 
+                }
+            }
+        }
+        
+        System.out.println(pq.peek());
+    }
+}
