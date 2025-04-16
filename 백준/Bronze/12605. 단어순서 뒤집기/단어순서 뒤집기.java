8634e0a4bae226562df6ab3c3abc70f5729277e7
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-16 23:00:36 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 176 ms, Memory: 18256 KB -BaekjoonHub

---
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+
+            Stack<String> stack = new Stack<>();
+
+            String[] input = sc.nextLine().split(" ");
+
+            for (String s : input) {
+
+                stack.push(s);
+
+            }
+
+            System.out.print("Case #" + (i + 1) + ": ");
+
+            while (!stack.isEmpty()) {
+
+                System.out.print(stack.pop() + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
+
