f54a5e268a6a791a6fbe6d8ebd1da7a35962573c
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-20 17:25:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 180 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        BigInteger c = sc.nextBigInteger();
+
+        BigInteger result = a.add(b).add(c);
+        System.out.println(result);
+    }
+}
