2651b492ae073957519ef7619bcf298756c467f0
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-27 17:00:51 +0900

    [level 2] Title: 피로도, Time: 4.62 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,18 +1,19 @@
 import java.lang.Math;
 
 class Solution {
-    int max = 0;
+    public static boolean visited[];
+    public static int max = 0;
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited = new boolean[dungeons.length];
-        dfs(k, dungeons, visited, 0);
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
             
         return max;
     }
-    public void dfs(int hp, int[][] dungeons, boolean[] visited,int count){
+    public static void dfs(int hp, int[][] dungeons,int count){
         for(int i=0; i<dungeons.length; i++){
             if(!visited[i] && hp>=dungeons[i][0]){
                 visited[i] = true;
-                dfs(hp-dungeons[i][1], dungeons, visited, count+1);
+                dfs(hp-dungeons[i][1], dungeons, count+1);
                 visited[i] = false;
             }
         }
