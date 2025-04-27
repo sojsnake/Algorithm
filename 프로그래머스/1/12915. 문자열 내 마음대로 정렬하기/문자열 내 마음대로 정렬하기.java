d1087be389454aeb1de74255c8f5d15ddb0c1c69
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-27 23:00:49 +0900

    Update 문자열 내 마음대로 정렬하기.java

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -23,3 +23,19 @@ class Solution {
         return answer;
     }
 }
+
+
+//-------------------------------------------------------------------
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (s1, s2) -> {
+            if (s1.charAt(n) == s2.charAt(n)) {
+                return s1.compareTo(s2);
+            }
+            return s1.charAt(n) - s2.charAt(n);
+        });
+        return strings;
+    }
+}
