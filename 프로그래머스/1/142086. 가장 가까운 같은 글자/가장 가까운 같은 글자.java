4368045c29434e8e3becb980ef8dac256fc7ec66
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-27 23:01:21 +0900

    Update 가장 가까운 같은 글자.java

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -17,3 +17,19 @@ class Solution {
         return answer;
     }
 }
+
+//--------------------------------------------
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            answer[i] = i-map.getOrDefault(ch,i+1);
+            map.put(ch,i);
+        }
+        return answer;
+    }
+}
