c8ea3172d48190efba640f6b5933bcdf41b14dc9
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-28 16:08:34 +0900

    Update K번째수.java

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -8,7 +8,8 @@ class Solution {
             int first = commands[i][0];
             int end = commands[i][1];
             int k = commands[i][2];
-            
+
+            //array 배열을 first-1 부터 end-1까지 잘라서 arr에 넣기
             int[] arr = Arrays.copyOfRange(array, first-1, end);
             Arrays.sort(arr);
             answer[i] = arr[k-1];
