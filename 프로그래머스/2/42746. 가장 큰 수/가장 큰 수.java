45cf50b6681f4917117c19c09cb081e57f38d1fb
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-04-28 19:05:54 +0900

    Update 가장 큰 수.java

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -8,13 +8,17 @@ class Solution {
             nums[i] = String.valueOf(numbers[i]);
         }
 
-        //문자열 배열에서 a, b 가져와서 
+        //문자열 배열에서 a, b 가져와서 비교
+        //b+a가 크면, b->a 순으로. a+b가 크면, a->b 순으로 정렬
+        //compareTo는 사전순 정렬
         Arrays.sort(nums, (a, b) -> (b + a).compareTo(a + b));
 
         // 정렬 후 가장 앞에 있는 값이 0이면, 전부 0이라는 의미
+        //이걸 안해주면 000이런 식으로 반환됨
         if (nums[0].equals("0")) return "0";
 
         StringBuilder answer = new StringBuilder();
+        //정렬된 nums 배열 돌면서 빌더에 추가
         for (String num : nums) {
             answer.append(num);
         }
