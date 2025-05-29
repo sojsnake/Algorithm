ba5c3a8ce32b50352d9a379d8483df4b2f02549a
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-05-29 17:44:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 210.96 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,27 +3,34 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
 
+        //set 사용해서 report 중복 제거
         Set<String> reportSet = new HashSet<>(Arrays.asList(report));
+        //각자 신고 당한 횟수 저장하는 map
         Map<String, Integer> reportCount = new HashMap<>();
-        Map<String, List<String>> iReportWho = new HashMap<>();
+        //각자 신고한 사람 리스트 저장하는 map
+        Map<String, List<String>> iReportList = new HashMap<>();
 
-        for(String r : reportSet){
-            String[] s = r.split(" ");
-            String from  = s[0];
-            String to = s[1];
+        //map 2개 완성
+        for(String rs : reportSet){
+            String[] re = rs.split(" ");
+            String from = re[0]; //신고한 사람
+            String to = re[1]; //신고 당한 사람
 
+            //신고 당한 횟수 저장
             reportCount.put(to, reportCount.getOrDefault(to, 0)+1);
 
-            if(!iReportWho.containsKey(from)){
-                iReportWho.put(from, new ArrayList<>());
+            //신고한 사람 리스트에 없으면
+            if(!iReportList.containsKey(from)){
+                iReportList.put(from, new ArrayList<>());
             }
-            iReportWho.get(from).add(to);
+            iReportList.get(from).add(to);
         }
 
+        //k 이상인 애들 찾아서 answer 증가
         for(int i=0; i<id_list.length; i++){
-            String reportee = id_list[i];
-            for(String r : iReportWho.getOrDefault(reportee, new ArrayList<>())){
-                if(reportCount.get(r) >= k){
+            String reporter = id_list[i];
+            for(String reportee : iReportList.getOrDefault(reporter, new ArrayList<>())){
+                if(reportCount.get(reportee) >= k){
                     answer[i]++;
                 }
             }
