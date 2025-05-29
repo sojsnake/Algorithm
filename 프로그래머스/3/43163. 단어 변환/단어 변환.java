dd1f62948ddee81f4e7c3ca2b2ace283a0cdba47
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-05-29 17:22:01 +0900

    [level 3] Title: 단어 변환, Time: 1.07 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,44 +3,44 @@ class Solution {
     class Word{
         String word;
         int count;
-        public Word(String word, int count){
+
+        Word(String word, int count) {
             this.word = word;
             this.count = count;
+
         }
     }
-
-    int getDiffCount(String word1, String word2){
+    int diffCount(String word1, String word2){
         int count = 0;
         for(int i=0; i<word1.length(); i++){
-            if(word1.charAt(i) != (word2.charAt(i)))
+            if(word1.charAt(i) != word2.charAt(i))
                 count++;
         }
         return count;
     }
-
     public int solution(String begin, String target, String[] words) {
-        //큐에 들어갈 타입을 Word로 지정해야 함
-        Queue<Word> queue = new ArrayDeque<>();
+        int answer = 0;
         Set<String> visited = new HashSet<>();
 
-        //큐에 넣고 방문 처리
-        queue.offer(new Word(begin, 0));
+        Queue<Word> queue = new LinkedList<>();
+        queue.add(new Word(begin, 0));
         visited.add(begin);
 
         while(!queue.isEmpty()){
             Word cur = queue.poll();
-            if(cur.word.equals(target))
+            if(target.equals(cur.word)){
                 return cur.count;
-            
+            }
             for(String word : words){
-                if(getDiffCount(cur.word, word) == 1){
-                    if(!visited.contains(word)) {
-                        queue.offer(new Word(word, cur.count + 1));
+                if(!visited.contains(word)){
+                    if(diffCount(word, cur.word)==1){
+                        queue.add(new Word(word, cur.count+1));
                         visited.add(word);
                     }
                 }
             }
         }
+
         return 0;
     }
 }
