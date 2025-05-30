04befacb9144236f71fd44f6e2de939e0874f3b1
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-05-30 11:36:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.64 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,39 @@
-//bfs - 큐, visited
+//bfs - boolean visited, queue
 import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-        int[] dr = new int[]{1, -1, 0, 0};
-        int[] dc = new int[]{0, 0, -1, 1};
-        
-        Queue<int[]> queue = new ArrayDeque<>();
+        int[] dr = new int[]{1,-1,0,0};
+        int[] dc = new int[]{0,0,1,-1};
         boolean[][] visited = new boolean[n][m];
-        //첫 값 넣기
-        queue.add(new int[]{0, 0, 1});
-        visited[0][0] = true;
+        Queue<int[]> queue = new ArrayDeque<>();
         
+        //첫 값 넣기
+        queue.add(new int[]{0,0,1});
+        visited[0][0] = true;
         
         while(!queue.isEmpty()){
             int[] cur = queue.poll();
+            
             int r = cur[0], c = cur[1], dist = cur[2];
             
-            //마지막 칸이면 return dist
-            if(r==n-1 && c == m-1)
+            if(r == n-1 && c == m-1)
                 return dist;
             
-            for(int d=0; d<4; d++){
-                int nr = r + dr[d], nc = c+dc[d];
+            for(int d = 0; d<4; d++){
+                int nr = r+dr[d], nc = c+dc[d];
                 
-                if(nr>=0&&nr<n&&nc>=0&&nc<m&&maps[nr][nc]==1){
+                //방문 여부를 나중에
+                if(nr>=0 && nr<n && nc>=0 && nc<m && maps[nr][nc] == 1){
                     if(!visited[nr][nc]){
-                        visited[nr][nc] = true;
                         queue.add(new int[]{nr, nc, dist+1});
+                        visited[nr][nc] = true;
                     }
                 }
             }
         }
+        
         
         return -1;
     }
