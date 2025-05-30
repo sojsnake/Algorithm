24e7a114acf55ffddad90c27460f9c1a03cde1a6
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-05-30 11:46:44 +0900

    [level 1] Title: 신고 결과 받기, Time: 206.45 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,41 +1,46 @@
+//map 2개, set 1개
+//<신고 당한 사람, 횟수>
+//<신고자, 신고한 리스트>
 import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
-
-        //set 사용해서 report 중복 제거
+        
+        //set으로 중복 없애기
         Set<String> reportSet = new HashSet<>(Arrays.asList(report));
-        //각자 신고 당한 횟수 저장하는 map
+        
+        //Map<신고 당한 사람, 횟수>
         Map<String, Integer> reportCount = new HashMap<>();
-        //각자 신고한 사람 리스트 저장하는 map
-        Map<String, List<String>> iReportList = new HashMap<>();
-
-        //map 2개 완성
-        for(String rs : reportSet){
-            String[] re = rs.split(" ");
-            String from = re[0]; //신고한 사람
-            String to = re[1]; //신고 당한 사람
-
-            //신고 당한 횟수 저장
-            reportCount.put(to, reportCount.getOrDefault(to, 0)+1);
-
-            //신고한 사람 리스트에 없으면
-            if(!iReportList.containsKey(from)){
-                iReportList.put(from, new ArrayList<>());
+        //<신고자, 신고한 리스트>
+        Map<String, List<String>> iReportWho = new HashMap<>();
+        
+        for(String s : reportSet){
+            String[] parts = s.split(" ");
+            String from = parts[0]; //신고자
+            String to = parts[1]; //신고 당한 사람
+            
+            //map1 완성
+            reportCount.put(to, reportCount.getOrDefault(to, 0) + 1);
+            
+            //map2 완성
+            if(!iReportWho.containsKey(from)){
+                iReportWho.put(from, new ArrayList<>());
             }
-            iReportList.get(from).add(to);
+            //리스트에 추가
+            iReportWho.get(from).add(to);
         }
-
-        //k 이상인 애들 찾아서 answer 증가
+        
+        //answer 완성
         for(int i=0; i<id_list.length; i++){
             String reporter = id_list[i];
-            for(String reportee : iReportList.getOrDefault(reporter, new ArrayList<>())){
-                if(reportCount.get(reportee) >= k){
+            //reporter에 해당하는 리스트 가져와서 각 신고 당한사람별 count가 k이상
+            for(String reportee : iReportWho.getOrDefault(reporter, new ArrayList<>())){
+                if(reportCount.get(reportee) >= k)
                     answer[i]++;
-                }
             }
         }
-
+        
+        
         return answer;
     }
 }
