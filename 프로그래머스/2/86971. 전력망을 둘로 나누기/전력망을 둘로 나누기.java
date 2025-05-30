bf7a0751f9b862b8d4b98bd8e001a67341d8545c
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-05-30 09:30:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.52 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,40 +1,40 @@
+//트리 맵으로 그래프 구현하고 양방향 연결
+//dfs count+=dfs()
 import java.util.*;
 class Solution {
     int answer;
-    Map<Integer, List<Integer>> map;
+    Map<Integer, List<Integer>> graph = new HashMap<>();
     boolean[] visited;
-
+    
     public int solution(int n, int[][] wires) {
         answer = n;
-        map = new HashMap<>();
         visited = new boolean[n+1];
-
+        
         for(int i=1; i<=n; i++){
-            map.put(i, new ArrayList<>());
+            graph.put(i, new ArrayList<>());
         }
-
+        
         for(int[] wire : wires){
-            map.get(wire[0]).add(wire[1]);
-            map.get(wire[1]).add(wire[0]);
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
         }
-
+        
         dfs(1, n);
-
+        
         return answer;
     }
-
     int dfs(int cur, int n){
         int count = 1;
         visited[cur] = true;
-
-        for(int next : map.get(cur)){
+        for(int next : graph.get(cur)){
             if(!visited[next]){
                 count += dfs(next, n);
             }
+            
         }
-
-        answer = Math.min(answer, Math.abs(n - count*2));
-
+        
+        answer = Math.min(answer, Math.abs(n-count*2));
+        
         return count;
     }
 }
