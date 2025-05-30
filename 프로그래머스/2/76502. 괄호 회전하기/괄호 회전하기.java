246e945cf7a1ec78967d4d1cae19dcb6de6382e7
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-05-30 10:27:50 +0900

    [level 2] Title: 괄호 회전하기, Time: 31.70 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,38 +1,41 @@
+//스택 isValid 메서드
 import java.util.*;
 class Solution {
     boolean isValid(String s){
         Deque<Character> stack = new ArrayDeque<>();
-        for(char c : s.toCharArray()){
-            //오른쪽 괄호이면 스택에 추가
-            if(c == '(' || c == '[' || c=='{'){
-                stack.push(c);
-            }
-            //왼쪽 괄호가 들어올 때
+        
+        for(char current : s.toCharArray()){
+            //여는 괄호가 들어오면 스택에 추가
+            if(current == '(' || current == '{' || current == '[')
+                stack.push(current);
+            
+            //닫는 괄호가 들어올 때
             else{
-                if(stack.isEmpty()){
+                if(stack.isEmpty())
                     return false;
-                }
 
                 char target = stack.pop();
-                if((target=='(' && c!=')')
-                        || (target=='{' && c!='}')
-                        || (target=='[' && c!=']')){
+                if((target == '(' && current != ')')
+                  || (target == '{' && current != '}')
+                  || (target == '[' && current != ']'))
                     return false;
-                }
             }
+                
         }
+        
         return stack.isEmpty();
     }
-
+    
     public int solution(String s) {
         int answer = 0;
         String doubleS = s+s;
+        
         for(int i=0; i<s.length(); i++){
-            if(isValid(doubleS.substring(i, s.length()+i))){
+            if(isValid(doubleS.substring(i, i+s.length())))
                 answer++;
-            }
         }
-
+        
+        
         return answer;
     }
 }
