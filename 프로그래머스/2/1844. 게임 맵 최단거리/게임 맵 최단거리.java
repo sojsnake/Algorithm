1c18c5467b7eb98d1dc601c14c27f8f4cb9aaf37
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-05-30 10:47:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.65 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,39 @@
+//bfs - 큐, visited
 import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
-        //dist 리턴
         int n = maps.length;
         int m = maps[0].length;
-
-        int dr[] = {1,-1,0,0};
-        int dc[] = {0,0,-1,1};
-
-        //bfs -> visited, 큐 필요
-        boolean[][] visited = new boolean[n][m];
+        int[] dr = new int[]{1, -1, 0, 0};
+        int[] dc = new int[]{0, 0, -1, 1};
+        
         Queue<int[]> queue = new ArrayDeque<>();
-
+        boolean[][] visited = new boolean[n][m];
         //첫 값 넣기
-        queue.add(new int[]{0,0,1});
+        queue.add(new int[]{0, 0, 1});
         visited[0][0] = true;
-
+        
+        
         while(!queue.isEmpty()){
-            //큐에서 값 꺼내기
             int[] cur = queue.poll();
-            int row = cur[0];
-            int col = cur[1];
-            int dist = cur[2];
-
-            //현재 위치가 오른쪽 아래이면 리턴 끝
-            if(row == n-1 && col == m-1)
+            int r = cur[0], c = cur[1], dist = cur[2];
+            
+            //마지막 칸이면 return dist
+            if(r==n-1 && c == m-1)
                 return dist;
             
             for(int d=0; d<4; d++){
-                //next row, next col
-                int nr = row + dr[d];
-                int nc = col + dc[d];
-
-                //범위에 들어오고 벽이 아닐때
-                if(nr>=0 && nr<n && nc >= 0 && nc <m && maps[nr][nc]==1){
+                int nr = r + dr[d], nc = c+dc[d];
+                
+                if(nr>=0&&nr<n&&nc>=0&&nc<m&&maps[nr][nc]==1){
                     if(!visited[nr][nc]){
-                        queue.add(new int[]{nr, nc, dist+1});
                         visited[nr][nc] = true;
+                        queue.add(new int[]{nr, nc, dist+1});
                     }
                 }
             }
         }
+        
         return -1;
     }
 }
