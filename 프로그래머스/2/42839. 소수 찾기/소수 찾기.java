63eb4f7693f172f3103472be8c6acba156f67a70
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-07-02 13:50:13 +0900

    [level 2] Title: 소수 찾기, Time: 21.70 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,4 +1,3 @@
-//소수찾기 dfs(백트래킹), isPrime, HashSet, visited
 import java.util.*;
 class Solution {
     Set<Integer> primeSet;
@@ -11,26 +10,24 @@ class Solution {
         return primeSet.size();
     }
     void dfs(String numbers, boolean[] visited, String cur){
-        //cur의 길이
         if(cur.length()!=0){
             int num = Integer.parseInt(cur);
-            if(isPrime(num)){
+            if(isPrime(num))
                 primeSet.add(num);
-            }
         }
+        
         for(int i=0; i<numbers.length(); i++){
             if(!visited[i]){
-                visited[i] = true;
-                dfs(numbers, visited, cur+numbers.charAt(i));
+                visited[i] =true;
+                dfs(numbers, visited, cur + numbers.charAt(i));
                 visited[i] = false;
             }
         }
     }
     boolean isPrime(int num){
-        //2보다 작은지 큰지로 나눠서
         if(num<2) return false;
         for(int i=2; i*i<=num; i++){
-            if(num % i ==0)
+            if(num%i==0)
                 return false;
         }
         return true;
