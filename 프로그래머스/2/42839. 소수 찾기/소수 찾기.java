a53256c2b8f45b3a100fc17e89d659d935032de2
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-07-02 10:23:48 +0900

    [level 2] Title: 소수 찾기, Time: 21.74 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,42 +1,37 @@
-//dfs, 백트래킹. HashSet, boolean visited, backtrack(), isPrime() 
+//소수찾기 dfs(백트래킹), isPrime, HashSet, visited
 import java.util.*;
 class Solution {
     Set<Integer> primeSet;
-    
     public int solution(String numbers) {
-        int answer = 0;
-        
         primeSet = new HashSet<>();
-        boolean[] visited = new boolean[numbers.length()];
         
-        backtrack(numbers, visited, "");
+        boolean[] visited = new boolean[numbers.length()];
+        dfs(numbers, visited, "");
         
         return primeSet.size();
     }
-    //dfs
-    void backtrack(String numbers, boolean[] visited, String cur){
-        //cur가 소수인지 판별
-        if(cur.length() !=0){
+    void dfs(String numbers, boolean[] visited, String cur){
+        //cur의 길이
+        if(cur.length()!=0){
             int num = Integer.parseInt(cur);
-            if(isPrime(num))
+            if(isPrime(num)){
                 primeSet.add(num);
+            }
         }
-        
-        //cur 만들기
         for(int i=0; i<numbers.length(); i++){
             if(!visited[i]){
                 visited[i] = true;
-                backtrack(numbers, visited, cur+numbers.charAt(i));
+                dfs(numbers, visited, cur+numbers.charAt(i));
                 visited[i] = false;
             }
         }
     }
-    
-    //소수판별
     boolean isPrime(int num){
-        if(num < 2) return false;
+        //2보다 작은지 큰지로 나눠서
+        if(num<2) return false;
         for(int i=2; i*i<=num; i++){
-            if(num%i==0) return false;
+            if(num % i ==0)
+                return false;
         }
         return true;
     }
