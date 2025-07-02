69070341f1b063e0c25ed886a6d66d6482da1c55
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-07-02 10:49:10 +0900

    [level 2] Title: 타겟 넘버, Time: 6.72 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,17 @@
-//dfs(numbers, target, index, cur)
+//dfs
 import java.util.*;
 class Solution {
     public int solution(int[] numbers, int target) {
         return dfs(numbers, target, 0, 0);
     }
-    
     int dfs(int[] numbers, int target, int index, int cur){
-        int sum = 0;
+        //종료조건
         if(index == numbers.length){
-            return (cur == target) ? 1:0;
+            return (target==cur) ? 1: 0;
         }
         
+        //sum
+        int sum = 0;
         sum += dfs(numbers, target, index+1, cur+numbers[index]);
         sum += dfs(numbers, target, index+1, cur-numbers[index]);
         
