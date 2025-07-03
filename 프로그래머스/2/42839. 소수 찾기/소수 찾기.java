3e10e637f23bad6e99b6af98051ebb68162baee3
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-07-03 15:35:14 +0900

    [level 2] Title: 소수 찾기, Time: 27.23 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,23 +3,23 @@ class Solution {
     Set<Integer> primeSet;
     public int solution(String numbers) {
         primeSet = new HashSet<>();
-        
         boolean[] visited = new boolean[numbers.length()];
         dfs(numbers, visited, "");
-        
+
         return primeSet.size();
     }
     void dfs(String numbers, boolean[] visited, String cur){
-        if(cur.length()!=0){
+        if(cur.length() != 0){
             int num = Integer.parseInt(cur);
-            if(isPrime(num))
+            if(isPrime(num)){
                 primeSet.add(num);
+            }
         }
         
         for(int i=0; i<numbers.length(); i++){
             if(!visited[i]){
-                visited[i] =true;
-                dfs(numbers, visited, cur + numbers.charAt(i));
+                visited[i] = true;
+                dfs(numbers, visited, cur+numbers.charAt(i));
                 visited[i] = false;
             }
         }
@@ -27,7 +27,7 @@ class Solution {
     boolean isPrime(int num){
         if(num<2) return false;
         for(int i=2; i*i<=num; i++){
-            if(num%i==0)
+            if(num%i ==0)
                 return false;
         }
         return true;
