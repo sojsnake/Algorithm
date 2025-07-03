1cfed6d5da816ec788e88ee4cf5fcdc4f1631e33
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-07-03 15:47:56 +0900

    [level 2] Title: 타겟 넘버, Time: 6.52 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 83.2 MB, 시간: 6.56 ms
+메모리: 75.1 MB, 시간: 6.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 15:17:44
+2025년 07월 03일 15:47:54
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -4,12 +4,10 @@ class Solution {
         return dfs(numbers, target, 0, 0);
     }
     int dfs(int[] numbers, int target, int index, int cur){
-        //종료조건
         if(index == numbers.length){
             return (target == cur) ? 1:0;
         }
         int sum = 0;
-        
         sum += dfs(numbers, target, index+1, cur+numbers[index]);
         sum += dfs(numbers, target, index+1, cur-numbers[index]);
         
